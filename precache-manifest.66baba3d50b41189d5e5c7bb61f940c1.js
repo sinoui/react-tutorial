f12b1cb81f8ff67987e21f29033b7808579284e0
刘进行 <jackingstarliu@gmail.com>
2019-05-07 18:49:32 +0800

    Updates

---
+++ precache-manifest.66baba3d50b41189d5e5c7bb61f940c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a74ad93fb173503c65e08c25a7e9ab4",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "5ae06d69cbd6cb8ebc03",
+    "url": "/react-tutorial/static/css/main.682ca21c.chunk.css"
+  },
+  {
+    "revision": "fb00c114efe13909479e",
+    "url": "/react-tutorial/static/js/2.db7ce681.chunk.js"
+  },
+  {
+    "revision": "5ae06d69cbd6cb8ebc03",
+    "url": "/react-tutorial/static/js/main.1e1db56f.chunk.js"
+  },
+  {
+    "revision": "2a4d04de927634e03e5b",
+    "url": "/react-tutorial/static/js/runtime~main.782a6e4d.js"
+  }
+]);
